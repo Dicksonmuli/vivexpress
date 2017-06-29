b6ea698d1bde645a39c4433d43e49eff0f2b1c80
dicksonmuli <dicksonmully6@gmail.com>
2017-06-29 12:59:57 +0300

    installing firebase and initializiting firebase in middleware(entry file)

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ app.get('/', function(req, res) {
 	res.send('Express working now');
 });
 
+var firebase = require('firebase');
+  require('firebase/auth');
+  require('firebase/database');
+  // Initialize Firebase for the application
+  var config = {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      databaseURL: process.env.databaseURL,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId
+    };
+
+  firebase.initializeApp(config);
+
 app.listen(app.get('port'), function() {
 	console.log('Express started on port 8080');
 });
