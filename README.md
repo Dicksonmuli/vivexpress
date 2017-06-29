29890a625cf6936aa1317924c235eda0dd538b25
dicksonmuli <dicksonmully6@gmail.com>
2017-06-29 16:33:37 +0300

    add a README with a description on how to install and run the app locally

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vivexpress  
+
+### Installation
+1. git clone https://github.com/curtello/uzi.git
+
+2. make sure node and npm (Node Packet Manager) is installed.    You can (follow this link to install on linux)[https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04]
+3. npm install
+4. node app.js
--- app.js
+++ app.js
@@ -10,18 +10,40 @@ app.get('/', function(req, res) {
 
 var firebase = require('firebase');
   require('firebase/auth');
-  require('firebase/database');
+  // require('firebase/database');
   // Initialize Firebase for the application
-  var config = {
-      apiKey: process.env.apiKey,
-      authDomain: process.env.authDomain,
-      databaseURL: process.env.databaseURL,
-      storageBucket: process.env.storageBucket,
-      messagingSenderId: process.env.messagingSenderId
-    };
+  // var config = {
+  //     apiKey: process.env.apiKey,
+  //     authDomain: process.env.authDomain,
+  //     databaseURL: process.env.databaseURL,
+  //     storageBucket: process.env.storageBucket,
+  //     messagingSenderId: process.env.messagingSenderId
+  //   };
 
-  firebase.initializeApp(config);
+  firebase.initializeApp({
+		// config,
+		serviceAccount: "./vivexpress-service-account.json",
+		databaseURL: "https://vivexpress-96b69.firebaseio.com"});
 
+var message = {text: "Hello world", timestamp: new Date().toString()};
+var ref = firebase.database().ref('node-client');
+var messageRef = ref.child('messages');
+var logsRef = ref.child('logs');
+
+logsRef.child(messageRef.key).set(message);
+
+messageRef.orderByKey().limitToLast(1).on('child_added', function (snap) {
+	logsRef.child('count').transaction(function (i) {
+		return i + 1;
+	})
+})
+
+// messageRef.push({
+// 	name: 'Chris',
+// 	admin: true,
+// 	count: 1,
+// 	text: 'I am admin'
+// })
 app.listen(app.get('port'), function() {
 	console.log('Express started on port 8080');
 });
